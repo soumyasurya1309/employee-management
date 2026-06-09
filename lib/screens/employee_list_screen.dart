13849fbe7581f8013916206cf8b842e81dd64fdb
soumyasurya1309 <suryawanshisoumya506@gmail.com>
2026-06-09 12:37:23 +0530

    Cloud push notifications working

--- lib/screens/employee_list_screen.dart
+++ lib/screens/employee_list_screen.dart
@@ -179,7 +179,7 @@ class _EmployeeListScreenState extends State<EmployeeListScreen> {
     return ListView.separated(
       padding: const EdgeInsets.fromLTRB(16, 0, 16, 80),
       itemCount: employees.length,
-      separatorBuilder: (_, __) => const SizedBox(height: 8),
+      separatorBuilder: (_, _) => const SizedBox(height: 8),
       itemBuilder: (context, index) {
         final employee = employees[index];
         return Slidable(
